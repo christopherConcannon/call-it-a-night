b2c794c30ee8a93afaf684c2c1696bf37b417ef2
xtophe xanon <xocon10@gmail.com>
2020-07-08 14:32:42 -0500

    capture form values for custom search and return coordinates/time object

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,8 +29,8 @@ function getUserCoords() {
 		coordObj.lat = crd.latitude;
 		coordObj.lon = crd.longitude;
 		// current moment object
-		coordObj.date = moment().format('YYYY-MM-DDTHH:mm:ss');
-		// coordObj.time = moment().format('hh:mm:ss');
+		coordObj.dateTimeStart = moment().format('YYYY-MM-DDTHH:mm:ss');
+		coordObj.dateTimeEnd = moment().add(6, 'h').format('YYYY-MM-DDTHH:mm:ss');
 
 		console.log(coordObj);
 
@@ -50,27 +50,38 @@ function getUserCoords() {
 	}
 }
 
-// GET SELECTED COORDINATES AND DATE/TIME
+// // GET SELECTED COORDINATES AND DATE/TIME
+
 function getCustomCoords() {
-	// // GET FORM FIELD INPUT FOR CITY
-	// const cityInput =
-	// // fetch request to OpenCage API
-	// const url = `${cityInput}`
+	// GET FORM FIELD INPUT FOR CITY
+	const cityInputVal = cityInputEl.value.trim();
+	console.log(cityInputVal);
+	const dateInputVal = dateInputEl.value;
+	console.log(dateInputVal);
+	const timeInputVal = timeInputEl.value.trim();
+	console.log(timeInputVal);
+  let dateTimeStart = `${dateInputVal} ${timeInputVal}`;
+  
+	// fetch request to OpenCage API
+  // format url
+	const openCageApiKey = '0c839a0f2d5a4c3192c2c08f5fd44dfc';
+	const openCageUrl = `https://api.opencagedata.com/geocode/v1/json?q=${cityInputVal}&key=${openCageApiKey}&pretty=1&no_annotations=1`;
 
-	// GET FORM FIELD INPUTS FOR DATE AND TIME
-	// current moment object
-	// coordObj.date = moment(date from form field)
-	// coordObj.time = moment time(time from form field)
+	fetch(openCageUrl)
+		.then(function(res) {
+			return res.json();
+		})
+		.then(function(data) {
+			coordObj.lat = data.results[0].geometry.lat;
+      coordObj.lon = data.results[0].geometry.lng;
+      coordObj.dateTimeStart = moment(dateTimeStart, 'MMM D, YYYY HH:mm A', true).format('YYYY-MM-DDTHH:mm:ss');
+      coordObj.dateTimeEnd = moment(dateTimeStart, 'MMM D, YYYY HH:mm A', true).add(6, 'h').format('YYYY-MM-DDTHH:mm:ss');
+      console.log(coordObj);
+      revealResultsContainer(coordObj);
+		});
 
-	// coordObj.lat = data.latitude;
-	// coordObj.lon = data.longitude;
-  // coordObj.date =
-  console.log(coordObj);
-
-	revealResultsContainer(coordObj);
+	
 }
-
-
 
 // function to synchronize revealResultsContainer with fetch requests so container will be on page before data is returned and sent to individual display<x>Results() functions, thus avoiding asynchronicity issues
 function revealResultsContainer(coordObj) {
@@ -98,7 +109,7 @@ function zomatoFetch(coordObj) {
 			if (res.ok) {
 				res.json().then(function(data) {
 					let restauData = data.nearby_restaurants;
-					
+
 					// pass API data to display function
 					displayRestauResults(restauData);
 				});
@@ -113,21 +124,23 @@ function zomatoFetch(coordObj) {
 		});
 }
 
-
 // fetch from Tix Master, passing in coordObj
 function tixMasterFetch(coordObj) {
 	const lat = coordObj.lat;
-	const lon = coordObj.lon;
+  const lon = coordObj.lon;
+  console.log(lat, lon);
+  const dateTimeStart = coordObj.dateTimeStart;
+  const dateTimeEnd = coordObj.dateTimeEnd;
 
 	// convert lat/lon to geohash using 3rd party script
-	const geoPoint = Geohash.encode(lat, lon, 7);
+  const geoPoint = Geohash.encode(lat, lon, 7);
+  console.log(geoPoint);
 
-  const tixAPIkey = 'wEkOlTafP8T1DEZZ4GWREy4AwGrWvuBx';
-  
-  // const tixMasterAPIUrl = `https://app.ticketmaster.com/discovery/v2/events.json?geoPoint=${geoPoint}&radius=10&localStartDateTime=2020-07-21T16:30:00,2020-07-21T23:30:00&apikey=${tixAPIkey}`;
-  // const tixMasterAPIUrl = `https://app.ticketmaster.com/discovery/v2/events.json?geoPoint=9v6s0j&radius=10&localStartDateTime=2020-07-21T16:30:00,2020-07-21T23:30:00&apikey=${tixAPIkey}`;
-  const tixMasterAPIUrl = `https://app.ticketmaster.com/discovery/v2/events.json?geoPoint=9v6s0j&radius=10&&apikey=${tixAPIkey}`;
+	const tixAPIkey = 'wEkOlTafP8T1DEZZ4GWREy4AwGrWvuBx';
 
+	// const tixMasterAPIUrl = `https://app.ticketmaster.com/discovery/v2/events.json?geoPoint=${geoPoint}&radius=10&localStartDateTime=${dateTimeStart},${dateTimeEnd}&apikey=${tixAPIkey}`;
+	// const tixMasterAPIUrl = `https://app.ticketmaster.com/discovery/v2/events.json?geoPoint=9v6s0j&radius=10&localStartDateTime=2020-07-21T16:30:00,2020-07-21T23:30:00&apikey=${tixAPIkey}`;
+	const tixMasterAPIUrl = `https://app.ticketmaster.com/discovery/v2/events.json?geoPoint=9v6s0j&radius=10&&apikey=${tixAPIkey}`;
 
 	fetch(tixMasterAPIUrl)
 		.then(function(res) {
@@ -155,62 +168,59 @@ function tixMasterFetch(coordObj) {
 }
 
 function displayRestauResults(restauData) {
-  console.log('getZomatoData -> nearbyRestaurants', restauData);
-  
-  // populate cards
-  let innerHTML = '';
-  const index = ['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
-  for (var i = 0; i <= 6; i++) {
-    // url encode special characters for query
+	console.log('getZomatoData -> nearbyRestaurants', restauData);
+
+	// populate cards
+	let innerHTML = '';
+	const index = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven' ];
+	for (var i = 0; i <= 6; i++) {
+		// url encode special characters for query
 		const address = restauData[i].restaurant.location.address;
 		const encodedAddress = address.replace(/ /g, '%20').replace(/,/g, '%2C');
-    
-    innerHTML += `
-    <div href="#${index[i]}!" class="carousel-item" data-id="restau-${i+1}">
+
+		innerHTML += `
+    <div href="#${index[i]}!" class="carousel-item" data-id="restau-${i + 1}">
     <div class="card">
       <div class="card-image">
         <img src="${restauData[i].restaurant.thumb !== ''
-        				? restauData[i].restaurant.thumb
-        				: './assets/images/restau-placeholder-img.jpg'}">
+			? restauData[i].restaurant.thumb
+			: './assets/images/restau-placeholder-img.jpg'}">
       </div>
       <div class="card-content">
         <p>${restauData[i].restaurant.name} -- ${restauData[i].restaurant.cuisines}</p>
       </div>
       <div class="card-action">
-        <a href="${restauData[i].restaurant.url}" class="site" target="_blank"><i class="material-icons">language</i></a>
+        <a href="${restauData[i].restaurant
+			.url}" class="site" target="_blank"><i class="material-icons">language</i></a>
         <a href="https://www.google.com/maps/dir/?api=1&destination=${encodedAddress}" class="location" target="_blank"><i class="material-icons">directions</i></a>
         <i class="material-icons fav-icon">favorite_border</i>
       </div>
     </div>
     </div>
-    `
-  }
-  restauCarouselEl.innerHTML = innerHTML;
-  // initialize Materialize Carousel
-  M.Carousel.init($('#restau-carousel.carousel'));
-  
+    `;
+	}
+	restauCarouselEl.innerHTML = innerHTML;
+	// initialize Materialize Carousel
+	M.Carousel.init($('#restau-carousel.carousel'));
 
-  // TODO
+	// TODO
 	// set data-id attr for fav tracking
-	
 }
 
 function displayEventResults(eventData) {
-  // populate cards
-  console.log('tixMasterFetch -> nearbyEvents', eventData);
-  
-  // populate cards
-  let innerHTML = '';
-  const index = ['one', 'two', 'three', 'four', 'five', 'six', 'seven'];
-  // getting error when there are less than 7 results cannot read '_embedded' of undefined.  need to set condition
-  for (var i = 0; i <= 6; i++) {
+	// populate cards
+	console.log('tixMasterFetch -> nearbyEvents', eventData);
 
-    let eventLat = eventData[i]._embedded.venues[0].location.latitude;
+	// populate cards
+	let innerHTML = '';
+	const index = [ 'one', 'two', 'three', 'four', 'five', 'six', 'seven' ];
+	// getting error when there are less than 7 results cannot read '_embedded' of undefined.  need to set condition
+	for (var i = 0; i <= 6; i++) {
+		let eventLat = eventData[i]._embedded.venues[0].location.latitude;
 		let eventLon = eventData[i]._embedded.venues[0].location.longitude;
- 
-    
-    innerHTML += `
-    <div href="#${index[i]}!" class="carousel-item" data-id="event-${i+1}">
+
+		innerHTML += `
+    <div href="#${index[i]}!" class="carousel-item" data-id="event-${i + 1}">
     <div class="card">
       <div class="card-image">
         <img src="${eventData[i].images[0].url}">
@@ -219,19 +229,19 @@ function displayEventResults(eventData) {
         <p>${eventData[i].name}</p>
       </div>
       <div class="card-action">
-        <a href="${eventData[i].url}" class="site" target="_blank"><i class="material-icons">language</i></a>
+        <a href="${eventData[i]
+			.url}" class="site" target="_blank"><i class="material-icons">language</i></a>
         <a href="https://www.google.com/maps/dir/?api=1&destination=${eventLat},${eventLon}" class="location" target="_blank"><i class="material-icons">directions</i></a>
         <i class="material-icons fav-icon">favorite_border</i>
       </div>
     </div>
     </div>
-    `
-  }
-  eventCarouselEl.innerHTML = innerHTML;
-  // initialize Materialize Carousel
-  M.Carousel.init($('#event-carousel.carousel'));
+    `;
+	}
+	eventCarouselEl.innerHTML = innerHTML;
+	// initialize Materialize Carousel
+	M.Carousel.init($('#event-carousel.carousel'));
 
-	
 	// set data-id attr for fav tracking
 	// add event listener to fav icon...callbck addToFavs()
 	// set data-id attr for fav tracking
@@ -286,4 +296,3 @@ searchFormSubmitBtnEl.addEventListener('click', getCustomCoords);
 
 // event listener for fav icon
 resultsWrapperEl.addEventListener('click', buildFav);
-
